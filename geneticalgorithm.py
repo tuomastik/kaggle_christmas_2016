e20dca4a849a7fc7f33e26c883f0d6479dba63cf
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2016-12-27 11:40:46 +0200

    Added gift_weight_init_method, n_generations_weight_resample and

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -11,33 +11,56 @@ GIFTS_PER_BAG_INITIALLY = np.diff(np.linspace(
     start=0, stop=utils.N_GIFTS, num=utils.N_BAGS + 1, dtype=int))
 
 
+class SelectionMethod:
+    def __init__(self):
+        pass
+    truncation = 1
+    roulette_wheel = 2
+
+
+class GiftWeightInitMethod:
+    def __init__(self):
+        pass
+    expected_mean = 'numerical_mean'
+    expected_median = 'numerical_median'
+    expected_analytical = 'analytical'
+    sample_from_distr = 'sample_from_distribution'
+
+
 class GeneticAlgorithm:
     SAVED_SOLUTIONS_FOLDER = 'ga_solutions'
 
-    def __init__(self, population_size=50):
-        self.individuals = np.array([SolutionCandidate() for _ in
-                                     range(population_size)])
+    def __init__(self, population_size=50,
+                 gift_weight_init_method=GiftWeightInitMethod.expected_mean):
+        self.calculate_expected_weights_if_needed(gift_weight_init_method)
+        self.individuals = np.array([SolutionCandidate(gift_weight_init_method)
+                                     for _ in range(population_size)])
         self.best_individual = None
 
     def train(self, n_generations=1000, for_reproduction=0.1,
-              mutation_rate=0.01):
+              mutation_rate=0.01, selection_method=SelectionMethod.truncation,
+              n_generations_weight_resample=None):
         # Round the number of parents to be selected to an even number so that
         # we can execute bio-inspired operators in a pair wise manner.
         n_parents = utils.round_down_to_even(for_reproduction *
                                              len(self.individuals))
         children_per_parent = int(1.0 / for_reproduction)
         for generation in range(1, n_generations + 1):
-            # fittest = self.parent_selection_roulette_wheel(n_parents)
-            fittest = self.parent_selection_truncation(n_parents)
 
-            print("")
-            print("Rewards of the new parents")
+            parents = self.select_parents(selection_method, n_parents)
+
+            if (n_generations_weight_resample is not None and
+                    generation % n_generations_weight_resample == 0):
+                print("\nResampling gift weights for the new parents")
+                parents = self.resample_weights(parents)
+
+            print("\nRewards of the new parents")
             print("--------------------------")
-            for f in fittest:
+            for f in parents:
                 print("%.3f" % f.reward)
 
             self.individuals = self.breed_mutation(
-                parents=fittest, children_per_parent=children_per_parent,
+                parents=parents, children_per_parent=children_per_parent,
                 mutation_rate=mutation_rate)
             self.calculate_rewards()
             self.find_best_individual()
@@ -61,10 +84,18 @@ class GeneticAlgorithm:
 
     def parent_selection_truncation(self, n_parents):
         rewards = [i.reward for i in self.individuals]
-        # Find indices of n_best solution rewards
+        # Find indices of solutions with best rewards
         best_ix = np.argpartition(rewards, -n_parents)[-n_parents:]
-        fittest_solutions = self.individuals[best_ix]
-        return fittest_solutions
+        parents = self.individuals[best_ix]
+        return parents
+
+    def select_parents(self, selection_method, n_parents):
+        if selection_method == SelectionMethod.truncation:
+            return self.parent_selection_truncation(n_parents)
+        elif selection_method == SelectionMethod.roulette_wheel:
+            return self.parent_selection_roulette_wheel(n_parents)
+        else:
+            raise(Exception("Unknown selection_method"))
 
     @staticmethod
     def breed_mutation(parents, children_per_parent, mutation_rate):
@@ -100,17 +131,38 @@ class GeneticAlgorithm:
         variables_save = {'best_individual': self.best_individual}
         np.save(file_path, variables_save)
 
+    @staticmethod
+    def calculate_expected_weights_if_needed(gift_weight_init_method):
+        if gift_weight_init_method in [
+                GiftWeightInitMethod.expected_mean,
+                GiftWeightInitMethod.expected_median,
+                GiftWeightInitMethod.expected_analytical]:
+            utils.set_expected_gift_weights(n_observations_per_gift=1000000,
+                                            approach=gift_weight_init_method)
+
+    @staticmethod
+    def resample_weights(individuals):
+        for i, individual in enumerate(individuals):
+            for j, bag in enumerate(individual.bags):
+                for k, gift in enumerate(bag.gifts):
+                    individuals[i].bags[j].gifts[k].weight = (
+                        utils.GIFT_WEIGHT_DISTRIBUTIONS[
+                            gift.id_label.split('_')[0]]())
+                individuals[i].bags[j].calculate_weight()
+            individuals[i].calculate_reward()
+        return individuals
+
 
 class SolutionCandidate:
-    def __init__(self):
+    def __init__(self, gift_weight_init_method):
         # print("Creating solution...")
-        self.bags = self.initialize_bags()
-        self.reward = 0
+        self.bags = self.initialize_bags(gift_weight_init_method)
+        self.reward = 0.0
         self.calculate_reward()
         # print("  - Reward: %s" % self.reward)
 
     @staticmethod
-    def initialize_bags():
+    def initialize_bags(gift_weight_init_method):
         # print("Initializing bags...")
         bags = []
         random_row_indices = np.random.permutation(utils.N_GIFTS)
@@ -119,15 +171,27 @@ class SolutionCandidate:
             extracted_gifts = utils.GIFTS_DF.iloc[random_row_indices[:n_gifts]]
             random_row_indices = random_row_indices[n_gifts:]
             for row_ix, gift in extracted_gifts.iterrows():
+                if gift_weight_init_method in [
+                        GiftWeightInitMethod.expected_mean,
+                        GiftWeightInitMethod.expected_median,
+                        GiftWeightInitMethod.expected_analytical]:
+                    gift_weight = utils.EXPECTED_GIFT_WEIGHTS[
+                        gift['gift_type']]
+                elif (gift_weight_init_method ==
+                      GiftWeightInitMethod.sample_from_distr):
+                    gift_weight = utils.GIFT_WEIGHT_DISTRIBUTIONS[
+                        gift['gift_type'].lower()]()
+                else:
+                    raise(Exception("Unknown gift_weight_init_method"))
                 bag.add_new_gift(
-                    weight=utils.EXPECTED_GIFT_WEIGHTS[gift['gift_type']],
+                    weight=gift_weight,
                     id_label=gift['GiftId'],
                     running_number=row_ix)
             bags.append(bag)
         return bags
 
     def calculate_reward(self):
-        self.reward = 0
+        self.reward = 0.0
         for bag in self.bags:
             if len(bag.gifts) >= 3 and bag.weight <= utils.MAX_BAG_WEIGHT:
                 self.reward += bag.weight
@@ -189,6 +253,11 @@ class Bag:
     def remove_gift(self, gift):
         self.gifts.remove(gift)
         self.weight -= gift.weight
+
+    def calculate_weight(self):
+        self.weight = 0.0
+        for gift in self.gifts:
+            self.weight += gift.weight
 
 
 class Gift:
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-import utils
 import geneticalgorithm
 
 if __name__ == '__main__':
     seed = 1122345
     np.random.seed(seed)
-
-    utils.set_expected_gift_weights(n_observations_per_gift=1000000,
-                                    approach='numerical_median')
-    ga = geneticalgorithm.GeneticAlgorithm(population_size=100)
-    ga.train(n_generations=10000, for_reproduction=0.1, mutation_rate=1)
+    ga = geneticalgorithm.GeneticAlgorithm(
+        population_size=200, gift_weight_init_method=(
+            geneticalgorithm.GiftWeightInitMethod.sample_from_distr))
+    ga.train(n_generations=10000, for_reproduction=0.1, mutation_rate=20,
+             n_generations_weight_resample=1)
